40a8041bf6a6412f8200ead2137b5d1d8752db8b
saraaguilarb <122842897+saraaguilarb@users.noreply.github.com>
2023-01-22 22:02:14 -0600

    Update index.js

--- Tarea1_Desarrollo/index.js
+++ Tarea1_Desarrollo/index.js
@@ -2,10 +2,7 @@ var http = require('http');
 var url = require('url');
 
 http.createServer( function(req, res){
-    
-    // Por que es q en el ejemplo?
-    // Por que a operación de leer los parametros de una url se le dice 
-    // Query the url (Consulta los parametros de la ulr)
+   
     var q = url.parse(req.url, true);
     var datos = q.query; 
 
